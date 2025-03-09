2a5874aebf2d9ef7a9f21fbb321c04691af0f6a6
Dayles1 <sololeveling0025@gmail.com>
2025-03-09 17:30:37 +0500

    +uploadFile to abstract Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function uploadFile($file,$path='uploads')
+    {
+        $fileName=md5(time().$file->getFilename()).'.'.$file->getClientOriginalExtension();
+        return $file->storeAs($path,$fileName,'public');
+    }
+    
 }
