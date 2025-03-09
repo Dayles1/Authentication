97d9de1cc79b653482a8b67f1fc35be613311cd9
Dayles1 <sololeveling0025@gmail.com>
2025-03-09 18:19:21 +0500

    +path to users table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
+        $user=User::create([
+
+        ]);
     }
 }
