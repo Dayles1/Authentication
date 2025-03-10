fda572b41d66b6503b0f33d6a0bfd3a4f68c0136
Dayles1 <sololeveling0025@gmail.com>
2025-03-10 21:37:18 +0500

    +Send email to Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Mail\SendEmailNotification;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
@@ -11,13 +13,19 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
 
-        $avatar=$request->file('image');
+        $image=$request->file('image');
+        $avatar=$this->uploadFile($image,'avatar');
 
         $user=User::create([
             'name'=>$request->name,
             'email'=>$request->email,
             'avatar'=>$request->avatar,
             'password'=>bcrypt($request->password),
+        ]);
+        Mail::to($request->email)->send(new SendEmailNotification());
+        return response()->json([
+            'success'=>true,
+            'message'=>'Emailingzini tekshiring'
         ]);
     }
 }
