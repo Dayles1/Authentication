576dbad38c9e21cc108dbefea7d53f9661d78ef4
Dayles1 <sololeveling0025@gmail.com>
2025-03-10 21:12:30 +0500

    +++Auth register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 
@@ -9,8 +10,14 @@ class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        $user=User::create([
 
+        $avatar=$request->file('image');
+
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'avatar'=>$request->avatar,
+            'password'=>bcrypt($request->password),
         ]);
     }
 }
