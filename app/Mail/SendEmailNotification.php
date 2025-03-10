35a1d18ffb836c4bbe991ccd542036fea9b24e4b
Dayles1 <sololeveling0025@gmail.com>
2025-03-10 21:38:36 +0500

    i wrode SendEmail../s content

--- app/Mail/SendEmailNotification.php
+++ app/Mail/SendEmailNotification.php
@@ -37,7 +37,7 @@ class SendEmailNotification extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.verify',
         );
     }
 
